c4cee9c254ac991d861efeab5e60913a9aee1717
Wang Chunsheng <chunshengster@gmail.com>
2012-03-20 17:09:49 +0800

    修改 MapKitPlug，适用于PhoneGap 1.5 Cordova

--- iPhone/MapKitPlug/src/MapKit.h
+++ iPhone/MapKitPlug/src/MapKit.h
@@ -7,14 +7,14 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-#ifdef PHONEGAP_FRAMEWORK
-    #import <PhoneGap/PGPlugin.h>
+#ifdef CORDOVA_FRAMEWORK
+    #import <Cordova/CDVPlugin.h>
 #else
-    #import "PGPlugin.h"
+    #import "CDVPlugin.h"
 #endif
 
 
-@interface MapKitView : PGPlugin <MKMapViewDelegate> 
+@interface MapKitView : CDVPlugin <MKMapViewDelegate> 
 {
 }
 
